817b2f51d4ef2a66727234490ba32bfa8a3dd9a0
RuchirBo <ruchirb@rocketmail.com>
2025-04-22 22:25:02 -0400

    Small update to getAllItemsInOrder

--- order.mjs
+++ order.mjs
@@ -55,7 +55,7 @@ const updateOrder = async(orderID, itemID, quantity) => {
 
 // returns all items in an order
 const getAllItemsInOrder = async(orderID) => {
-    const o_query = `SELECT i.ItemName, i.Price, od.Quantity FROM CustomerOrder o
+    const o_query = `SELECT i.ItemID, i.ItemName, i.Price, od.Quantity FROM CustomerOrder o
     INNER JOIN OrderDetails od
     ON o.OrderID = od.OrderID
     INNER JOIN Item i
