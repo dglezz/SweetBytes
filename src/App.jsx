663b7a0d2523ca7aea132e4ee7cc1bb25fca6e8c
ishasingh615 <isha.singh.usa@gmail.com>
2025-04-25 22:15:55 -0400

    finished environment setup and added backend server (node.js and express) to connect to database

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import db from "./db.js"
+import db from "/db.js"
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ src/home.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+function About() {
+    return (
+        <div className="text-container">
+            <h2>Sweet Bytes Bakery</h2>
+            <button>
+            Update About
+            </button>
+        </div>
+    );
+}
+
+export default About;
